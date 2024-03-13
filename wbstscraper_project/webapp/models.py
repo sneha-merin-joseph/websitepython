95d34c79e470252ee88c85fce851e5f971490133
sneha joseph <snehajoseph580@gmail.com>
2024-03-13 11:14:46 +0530

    add

---
+++ wbstscraper_project/webapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Links(models.Model):
+
+    def __str__(self):
+        return  self.stringname
+
+    address=models.CharField(max_length=500,null=True,blank=True)
+    stringname=models.CharField(max_length=500,null=True,blank=True)
